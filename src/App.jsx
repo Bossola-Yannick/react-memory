3aae3fd16df57097ff42a30545ae7ba0846d6592
Bossola-Yannick <bossolay@hotmail.fr>
2025-05-22 11:47:09 +0200

    feat: structure project

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import "./App.css";
+import Button from "./components/Button/Button";
+import Cards from "./components/Cards/Cards";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <Cards />
+        <Button label="Lancer la Partie !" />
+      </div>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Cards() {
+  return <h2>Hello Card</h2>;
+}
+
+export default Cards;
